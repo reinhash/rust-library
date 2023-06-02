e9a82891633b66eb1c8606b1492e96e9d3d1bbb3
reinhas <reinhard.scheuerle@gmail.com>
2023-06-02 18:27:26 +0200

    compare different sizes in loop, remove overhead of creating python object from rust code which falsifies speed comparision

--- main.py
+++ main.py
@@ -1,28 +1,37 @@
+"""
+Developed with maturin.
+
+to compile the rust code, run:
+    maturin develop
+"""
 import time
-from functools import wraps
 
 import rust_library
 
+ELEMENTS = 1000 # around 1000, python and rust seem to be the same speed.
 
-def timer(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        start_time = time.time()
-        retval = func(*args, **kwargs)
-        print(f"the function took {time.time()-start_time} seconds")
-        return retval
-    return wrapper
+def timer(name):
+    def decorator_function(func):
+        def wrapper(*args, **kwargs):
+            start_time = time.time()
+            retval = func(*args, **kwargs)
+            print(f"{name}: the function took {time.time()-start_time} seconds")
+            return retval
+        return wrapper
+    return decorator_function
 
-@timer
+@timer('rust')
 def get_rust_array():
-    return rust_library.big_calculation()
+    return rust_library.big_calculation(ELEMENTS)
 
-@timer
+@timer('python')
 def get_python_array():
-    return [x for x in range(1_000_000)]
+    _ = [x for x in range(ELEMENTS)]
+    return
 
 
-get_rust_array() # the function took 0.08529520034790039 seconds
-get_python_array() # the function took 0.041697025299072266 seconds
+for element in range(ELEMENTS):
+    print(f"Running with size: {element}")
+    get_rust_array()
+    get_python_array()
 
-# turns out python is faster for such a task.
--- src/lib.rs
+++ src/lib.rs
@@ -2,14 +2,12 @@ use pyo3::prelude::*;
 
 /// Formats the sum of two numbers as string.
 #[pyfunction]
-fn big_calculation() -> Py<PyAny> {
-    let mut big_array = vec![0; 1_000_000];
-    for i in 0..1_000_000 {
+fn big_calculation(elements: usize) {
+    let mut big_array = vec![0; elements];
+    for i in 0..elements {
         big_array[i] = i;
     }
-    return Python::with_gil(|py| {
-        big_array.to_object(py)
-    });
+    return;
 }
 
 /// A Python module implemented in Rust.
